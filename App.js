3cf0b76a8b9a8bc5f7897017fcf1984cac2e0385
Willian Rigo <w9rigo@gmail.com>
2020-04-19 16:53:52 -0300

    no message

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
-import { HomeScreen } from './src/components/screens/';
+import { HomeScreen, LoginScreen } from './src/components/screens/';
 require('./src/firebase');
 
 export default function App() {
