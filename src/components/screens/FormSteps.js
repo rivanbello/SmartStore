067214e1d3b1f2a51f1ed2ef4ca27edd31c3d06a
Willian Rigo <w9rigo@gmail.com>
2020-04-27 14:17:35 -0300

    forms steps

---
+++ src/components/screens/FormSteps.js
@@ -0,0 +1,51 @@
+const steps = [
+  {
+    label: 'Vamos começar pelo seu nome',
+    stepIndex: 1,
+    formItems: [
+      {
+        placeholder: 'Nome',
+        iconFamily: 'FontAwesome',
+        iconName: 'user-circle',
+      }
+    ]
+  },
+  {
+    label: 'Legal ###, agora insira seu número de telefone',
+    stepIndex: 2,
+    formItems: [
+      {
+        placeholder: 'Telefone',
+        iconFamily: 'FontAwesome',
+        iconName: 'phone-square',
+      }
+    ],
+    phoneNumber: true,
+  },
+  {
+    label: '###, insira seu e-mail',
+    stepIndex: 3,
+    formItems: [
+      {
+        placeholder: 'E-mail',
+        iconFamily: 'MaterialCommunityIcons',
+        iconName: 'email-outline',
+      }
+    ],
+    email: true,
+  },
+  {
+    label: '###, insira seu e-mail',
+    stepIndex: 4,
+    formItems: [
+      {
+        placeholder: 'Data de nascimento',
+        iconFamily: 'AntDesign',
+        iconName: 'calendar',
+      }
+    ],
+    datePicker: true,
+  },
+]
+
+export default steps;
--- src/components/screens/RegisterScreen.jsx
+++ src/components/screens/RegisterScreen.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Screen from './Screen';
 import { StackHeader } from '../headers';
 import { Text } from 'react-native';
 import { COLORS } from '../../constants';
 import { FormItem } from '../forms';
+import steps from './formSteps';
 
-const RegisterScreen = () => (
-  <Screen>
-    <StackHeader />
-    <Text style={styles.title}>Cadastro</Text>
-    <Text style={styles.stepLabel}>Vamos começar pelo seu nome</Text>
-    <FormItem
-    />
-  </Screen>
-);
+const RegisterScreen = ({ step: {  } = {} }) => {
+  const [stepIndex, setStepIndex] = useState(0);
+  const [getBackFunction, setGetBackFunction] = useState(() => () => {
+    if (stepIndex > 0) setStepIndex(stepIndex - 1);
+    // else navigation.navigate('login')
+  });
+  const nextStep = () => {
+    if (stepIndex < steps.length) setStepIndex(stepIndex + 1);
+    // else navigation.navigate ...
+  }
+
+  return (
+    <Screen>
+      <StackHeader />
+      <Text style={styles.title}>Cadastro</Text>
+      <Text style={styles.stepLabel}>Vamos começar pelo seu nome</Text>
+      <FormItem
+        Icon={{
+          component: Icons['FontAwesome'],
+          name: steps[stepIndex].iconName,
+          size: 20,
+        }}
+      />
+    </Screen>
+  )
+};
 
 const styles = {
   title: {
