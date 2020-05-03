257f9903c9feee45df936e344d6a521d1e477c80
Willian Rigo <w9rigo@gmail.com>
2020-05-03 01:25:56 -0300

    fix datepicker

--- src/components/forms/FormItem.jsx
+++ src/components/forms/FormItem.jsx
@@ -154,8 +154,8 @@ const FormItem = ({
           style={styles.date}
           onPress={() => setDatePickerOpen(true)}  
         >
-          {date.getDate().length > 1 ? date.getDate() : `0${date.getDate()}`}/
-          {date.getMonth().length > 1 ? date.getMonth() : `0${date.getMonth()}`}/
+          {date.getDate().toString().length > 1 ? date.getDate() : `0${date.getDate()}`}/
+          {(date.getMonth() + 1).toString().length > 1 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`}/
           {date.getFullYear()}
         </Text>
       }
