325dad629ca6fd2d333ad6ab8846c704e01ad1b8
Willian Rigo <w9rigo@gmail.com>
2020-05-04 03:20:50 -0300

    fix

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,41 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import { FlatList, View, StyleSheet } from "react-native";
+import React, { useState, useEffect, useContext } from 'react';
+import { FlatList, ScrollView, StyleSheet } from "react-native";
 import { Dimensions } from 'react-native';
 import all from '../../client/list'
 import ListHeader from './ListHeader';
 import Item from './Item';
+import { UserContext } from '../../context';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 
 const List = () => {
-  const [loading, setLoading] = useState(true);
   const [list, setList] = useState([]);
+  const [userInfo, setUserInfo] = useContext(UserContext);
   
   useEffect(() => {
     all({ pointOfSaleId: 1 }).then(response => {
-      setLoading(false);
       setList(response);
     })
   }, []);
+
   return (
-    <View style={styles.container}>
-      <ListHeader label="Bebidas" expandLabel="Ver tudo" style={styles.header}/>
-      <FlatList
-        // style={styles.container}
-        horizontal
-        data={list.slice(0, 10)}
-        // contentContainerStyle={styles.content}
-        keyExtractor={item => item.id}
-        renderItem={({item: { description, categoryName, price, slots } = {}} ) => (
-          <Item
-            style={styles.item} 
-            description={description}
-            category={categoryName}
-            price={price}
-            qty={slots && slots[0].quantity}/>
-        )}
-      />
-    </View>
+    <ScrollView style={styles.container}>
+      {userInfo.categories && userInfo.categories.map((category) => <> 
+        <ListHeader label={category} expandLabel="Ver tudo" style={styles.header}/>
+        <FlatList
+          style={styles.list}
+          horizontal
+          data={
+            list
+              .filter(({ categoryId, categoryName}) => 
+                category === `Categoria ${categoryId}`
+                || category === categoryName
+              )
+            .slice(0, 10)}
+          // contentContainerStyle={styles.content}
+          keyExtractor={item => item.id}
+          renderItem={({item: { description, categoryName, price, slots } = {}} ) => (
+            <Item
+              style={styles.item} 
+              description={description}
+              category={categoryName}
+              price={price}
+              qty={slots && slots[0].quantity}/>
+          )}
+        />
+      </>)}
+    </ScrollView>
   )
 };
 
@@ -44,6 +53,9 @@ export default List;
 const styles = StyleSheet.create({
   container: {
     marginTop: 30,
+  },
+  list: {
+    marginBottom: 10,
   },
   header: {
     marginBottom: 16,
