bea95e456fe4fd77e86085c046274d2c39574b04
Willian Rigo <w9rigo@gmail.com>
2020-05-04 23:34:15 -0300

    no message

--- src/components/screens/CategoryScreen.jsx
+++ src/components/screens/CategoryScreen.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Item from '../list/Item';
 import { StackHeader } from '../headers';
+import { COLORS } from '../../constants';
 import Screen from './Screen';
 import { Row } from '../layout';
-import { Dimensions, FlatList } from 'react-native';
+import { Dimensions, Text } from 'react-native';
 
 const screenWidth = Math.round(Dimensions.get('window').width);
 
@@ -11,14 +12,22 @@ const CategoryScreen = ({ route, navigation }) => {
   const list = route.params.list || [];
   const category = route.params.category || '';
   let rows = [];
-  for (let i = 4; i < list.length; i = i + 4) rows.push(list.slice(i - 4, i));
+  for (let i = 4; i < (list.length + 4); i = i + 4) {
+    if (list.length >= i) rows.push(list.slice(i - 4, i));
+    else rows.push(list.slice(i - 4, i - (i % list.length)))
+  }
   return (
   <Screen>
+    {console.warn(rows.length)}
+    <StackHeader
+      onPress={() => navigation.goBack() }
+    />
+    <Text style={styles.title}>{category}</Text>
     {rows.map((row) => (
-      <Row>
+      <Row style={{ justifyContent: 'flex-start' }}>
         {row.map(({ description, price, quantity }) => (
           <Item
-            style={styles.item} 
+            style={styles.item}
             description={description}
             // category={category}
             price={price}
@@ -31,17 +40,16 @@ const CategoryScreen = ({ route, navigation }) => {
 };
 
 const styles = {
+  title: {
+    fontWeight: 'bold',
+    fontSize: 19,
+    color: COLORS.primary,
+    marginTop: '8%',
+    marginBottom: '4%',
+  },
   item: {
     marginRight: (screenWidth - (74 * 4) - 40) / 3,
-  },
-  content: {
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-    // overFlow: 'hidden'
-    // flex: 1,
-    // maxWidth: '100%'
+    marginBottom: '2%',
   },
 }
 
